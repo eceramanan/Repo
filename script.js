6c9d10be75199f485ecb0e4c01cb77d077ab7204
eceramanan <84.ramanan@gmail.com>
2016-01-04 21:35:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function runAtRunTimeBeforeLoad(){
   var	db = window.sqlitePlugin.openDatabase({name: "NewsCast.db", createFromLocation: 1});
   db.transaction(function(tx) {
-		tx.executeSql("update ConfigValues set value='http://dev-newscast.pantheon.io/wp-json/posts1?filter[posts_per_page]=-1' where Key='PostURL';", [], function(tx, res) {
-  			alert('test delete 1');
+		tx.executeSql("update ConfigValues set value='http://dev-newscast.pantheon.io/wp-json/post1s?filter[posts_per_page]=-1' where Key='PostURL';", [], function(tx, res) {
+  			alert('test delete 12');
 		  });
 	});
   
